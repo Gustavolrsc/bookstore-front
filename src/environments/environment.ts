4b109d38a1867e1cbd79386774317a8645c4433f
Gustavolrsc <gustavolrsc@gmail.com>
2025-03-13 15:07:44 -0300

    categoria-read foi implementado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://bookstore-api-production-30d1.up.railway.app'
+  
 };
 
 /*
